51213bb6d71ad3e0fb7dfaf761c33f79bbc0d9c6
Doofitator <ashley@sharkey.id.au>
2019-01-22 09:45:26 +1100

    Add project files.

---
+++ MinesweeperV3/frm_main.vb
@@ -0,0 +1,8 @@
+﻿Public Class frm_main
+    Public button_size As Integer = 30
+    Public grid_size As Integer = 20
+
+    Private Sub frm_main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
