d854cce0cf558a458e60d50123dd2d45e25971ac
Doofitator <ashley@sharkey.id.au>
2019-01-22 10:20:08 +1100

    got a toolstrip & grid

--- MinesweeperV3/frm_main.Designer.vb
+++ MinesweeperV3/frm_main.Designer.vb
@@ -22,18 +22,87 @@ Partial Class frm_main
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frm_main))
+        Me.stp_info = New System.Windows.Forms.StatusStrip()
+        Me.tssb_menu = New System.Windows.Forms.ToolStripDropDownButton()
+        Me.tssl_ticker = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.tssl_remainingMines = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.NewGameToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SettingsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.stp_info.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'stp_info
+        '
+        Me.stp_info.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.tssb_menu, Me.tssl_ticker, Me.tssl_remainingMines})
+        Me.stp_info.Location = New System.Drawing.Point(0, 428)
+        Me.stp_info.Name = "stp_info"
+        Me.stp_info.Size = New System.Drawing.Size(800, 22)
+        Me.stp_info.TabIndex = 0
+        Me.stp_info.Text = "StatusStrip1"
+        '
+        'tssb_menu
+        '
+        Me.tssb_menu.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.tssb_menu.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem, Me.SettingsToolStripMenuItem, Me.NewGameToolStripMenuItem})
+        Me.tssb_menu.Image = CType(resources.GetObject("tssb_menu.Image"), System.Drawing.Image)
+        Me.tssb_menu.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tssb_menu.Name = "tssb_menu"
+        Me.tssb_menu.Size = New System.Drawing.Size(51, 20)
+        Me.tssb_menu.Text = "Game"
+        '
+        'tssl_ticker
+        '
+        Me.tssl_ticker.Name = "tssl_ticker"
+        Me.tssl_ticker.Size = New System.Drawing.Size(31, 17)
+        Me.tssl_ticker.Text = "0000"
+        '
+        'tssl_remainingMines
+        '
+        Me.tssl_remainingMines.Name = "tssl_remainingMines"
+        Me.tssl_remainingMines.Size = New System.Drawing.Size(31, 17)
+        Me.tssl_remainingMines.Text = "0000"
+        '
+        'NewGameToolStripMenuItem
+        '
+        Me.NewGameToolStripMenuItem.Name = "NewGameToolStripMenuItem"
+        Me.NewGameToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.NewGameToolStripMenuItem.Text = "New Game"
+        '
+        'SettingsToolStripMenuItem
+        '
+        Me.SettingsToolStripMenuItem.Name = "SettingsToolStripMenuItem"
+        Me.SettingsToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.SettingsToolStripMenuItem.Text = "Settings"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.AboutToolStripMenuItem.Text = "About"
         '
         'frm_main
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.stp_info)
         Me.MaximizeBox = False
         Me.Name = "frm_main"
         Me.Text = "Minesweeper"
+        Me.stp_info.ResumeLayout(False)
+        Me.stp_info.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents stp_info As StatusStrip
+    Friend WithEvents tssb_menu As ToolStripDropDownButton
+    Friend WithEvents tssl_ticker As ToolStripStatusLabel
+    Friend WithEvents tssl_remainingMines As ToolStripStatusLabel
+    Friend WithEvents SettingsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NewGameToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
 End Class
--- MinesweeperV3/frm_main.vb
+++ MinesweeperV3/frm_main.vb
@@ -1,8 +1,76 @@
 ﻿Public Class frm_main
     Public button_size As Integer = 30
     Public grid_size As Integer = 20
+    Public mines_amount As Integer = 50
+    Public arr_btns(grid_size, grid_size) As Button
+    Public mine_btns(grid_size, grid_size) As Integer
 
     Private Sub frm_main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.ClientSize = New Size(button_size * grid_size, (button_size * grid_size) + 22)
+        loadGrid()
+    End Sub
+
+    Function loadGrid()
+        For y = 0 To grid_size - 1
+            For x = 0 To grid_size - 1
+                arr_btns(x, y) = New Button()
+                With arr_btns(x, y)
+                    .Width = button_size
+                    .Height = button_size
+                    .Top = x * button_size
+                    .Left = y * button_size
+                    .TabStop = 0
+                    .Name = x & "_" & y
+                End With
+                Me.Controls.Add(arr_btns(x, y))
+                AddHandler arr_btns(x, y).Click, AddressOf mine
+            Next
+        Next
+        loadMines()
+        Dim tmr As New Timer
+        With tmr
+            .Enabled = True
+            .Interval = 1000
+        End With
+        AddHandler tmr.Tick, AddressOf addcount
+    End Function
+
+    Function loadMines()
+        For y = 0 To grid_size - 1
+            For x = 0 To grid_size - 1
+                mine_btns(x, y) = New Integer()
+                Randomize()
+
+            Next
+        Next
+    End Function
+
+    Sub addcount(sender As Object, e As EventArgs)
+        tssl_ticker.Text += 1
+    End Sub
+
+    Sub mine(sender As Object, e As EventArgs)
+        Console.WriteLine(CType(sender, Button).Name)
+    End Sub
+
+    Private Sub NewGameToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewGameToolStripMenuItem.Click
+        Dim ctrlList As New List(Of Button)
+        For Each control In Me.Controls
+            If Not TypeOf control Is StatusStrip Then
+                ctrlList.Add(control)
+            End If
+        Next
+        For Each control In ctrlList
+            Me.Controls.Remove(control)
+        Next
+        frm_main_Load(sender, e)
+    End Sub
+
+    Private Sub SettingsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SettingsToolStripMenuItem.Click
+        'show settings form to change grid size and amount of mines
+    End Sub
+
+    Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
 
     End Sub
 End Class
