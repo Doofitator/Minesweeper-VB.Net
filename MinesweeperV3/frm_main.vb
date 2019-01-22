12244061007f523746353af8fece10b5d322bb2e
Doofitator <ashley@sharkey.id.au>
2019-01-22 12:37:23 +1100

    Got mines

--- MinesweeperV3/frm_main.vb
+++ MinesweeperV3/frm_main.vb
@@ -3,9 +3,11 @@
     Public grid_size As Integer = 20
     Public mines_amount As Integer = 50
     Public arr_btns(grid_size, grid_size) As Button
-    Public mine_btns(grid_size, grid_size) As Integer
+    Public mine_btns As New List(Of Button)
 
     Private Sub frm_main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        mine_btns.Clear()
+        tssl_ticker.Text = 0
         Me.ClientSize = New Size(button_size * grid_size, (button_size * grid_size) + 22)
         loadGrid()
     End Sub
@@ -36,13 +38,19 @@
     End Function
 
     Function loadMines()
-        For y = 0 To grid_size - 1
-            For x = 0 To grid_size - 1
-                mine_btns(x, y) = New Integer()
-                Randomize()
-
-            Next
+        Dim btnsList As List(Of Button) = New List(Of Button)
+        For Each control In Me.Controls
+            If TypeOf control Is Button Then
+                btnsList.Add(control)
+            End If
         Next
+        For i = 0 To mines_amount
+            Randomize()
+            Dim mineBtn As Button = btnsList.Item(CInt(Math.Ceiling(Rnd() * (grid_size ^ 2))))
+            mine_btns.Add(mineBtn)
+            Console.Write(mineBtn.Name & "  -  ")
+        Next
+        Console.WriteLine()
     End Function
 
     Sub addcount(sender As Object, e As EventArgs)
@@ -50,7 +58,10 @@
     End Sub
 
     Sub mine(sender As Object, e As EventArgs)
-        Console.WriteLine(CType(sender, Button).Name)
+        Dim this As Button = CType(sender, Button)
+        If mine_btns.Contains(this) Then
+            this.BackColor = Color.Red
+        End If
     End Sub
 
     Private Sub NewGameToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewGameToolStripMenuItem.Click
